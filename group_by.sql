3d039c39d015f8261c1c107ad175286954c40fe6
akshayp-mangospring <akshayp@mangospring.com>
2022-02-12 22:27:54 +0530

    Indent Fixes

--- group_by.sql
+++ group_by.sql
@@ -1,25 +1,37 @@
 -- Query to count the number of books published by an author
 -- Largest count shown first
-SELECT author_fname, author_lname, COUNT(*)
+SELECT
+  author_fname,
+  author_lname,
+  COUNT(*)
 FROM books
   GROUP BY author_fname
   ORDER BY COUNT(*) DESC, author_lname, author_fname;
 
 -- Query to count the number of books released in a year.
 -- Largest count shown first
-SELECT release_year, COUNT(*)
+SELECT
+  release_year,
+  COUNT(*)
 FROM books
   GROUP BY release_year
   ORDER BY COUNT(*) DESC, release_year DESC;
 
 -- Query to Sum up the number of pages written by an author
-SELECT author_fname, author_lname, SUM(pages) AS 'Pages Written'
+SELECT
+  author_fname,
+  author_lname,
+  SUM(pages) AS 'Pages Written'
 FROM books
   GROUP BY author_fname, author_lname
   ORDER BY SUM(pages) DESC;
 
 -- Query to find the total number of pages written & the number of books written by an author
-SELECT author_fname, author_lname, SUM(pages) AS 'Pages Written', COUNT(*) AS 'Books Written'
+SELECT
+  author_fname,
+  author_lname,
+  SUM(pages) AS 'Pages Written',
+  COUNT(*) AS 'Books Written'
 FROM books
   GROUP BY author_fname, author_lname
   ORDER BY SUM(pages) DESC, COUNT(*) DESC;
