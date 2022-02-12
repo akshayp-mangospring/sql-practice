b270ae60f610d69e44be1d21a3a64a954df48be6
akshayp-mangospring <akshayp@mangospring.com>
2022-02-12 23:16:44 +0530

    Add aggregate functions exercise solutions

---
+++ aggregate_funcs_exercise.sql
@@ -0,0 +1,48 @@
+-- Total Number of Books in DB
+-- Using Sum & Count
+SELECT SUM(b.Count) AS 'Total Books'
+FROM (
+  SELECT COUNT(*) AS 'Count'
+  FROM books
+  GROUP BY author_fname, author_lname
+) AS b;
+
+-- Simple approach of Count
+SELECT COUNT(*) AS 'Total Books'
+FROM books;
+
+-- How many books were released in each year
+SELECT
+  release_year,
+  COUNT(*) AS 'Book Release Count'
+FROM books
+GROUP BY release_year;
+
+-- Total number of Books in Stock
+SELECT SUM(stock_qty) AS 'Books in Stock'
+FROM books;
+
+-- Find the Average release_year for each Author
+SELECT
+  author_fname AS 'First Name',
+  author_lname AS 'Last Name',
+  AVG(release_year) AS 'Avg Release Year'
+FROM books
+GROUP BY author_fname, author_lname
+ORDER BY AVG(release_year);
+
+-- Full name of the Author who wrote the longest book
+SELECT
+  CONCAT_WS(' ', author_fname, author_lname) AS 'Author',
+  pages
+FROM books
+ORDER BY pages DESC
+LIMIT 1;
+
+-- Average number of pages in a year, Number of books in a year
+SELECT
+  release_year AS 'year',
+  COUNT(*) AS '# books',
+  AVG(pages) AS 'avg pages'
+FROM books
+GROUP BY release_year;
